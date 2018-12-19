4a091c41945d7ed91d18d2b84fdf1b64d9aa0c7a
Miguel Ravagnani <miguel.ravagnani@gmail.com>
2018-12-19 13:17:07 -0200

    faceloop.py 19/12/2018

--- faceloop.py
+++ faceloop.py
@@ -6,6 +6,28 @@ import argparse
 import time
 import dlib
 import numpy as np
+"""
+def printBox(index, X, Y, W, H, img, aux, dim, boxDrawing):
+    for i in range(0, index+1):
+        dim.append(i)
+        X.append(i)
+        Y.append(i)
+        W.append(i)
+        H.append(i)
+
+    dim[aux] = face_utils.rect_to_bb(boxDrawing)
+    (X[index], Y[index], W[index], H[index]) = dim[index]
+
+    cv2.rectangle(img, (X[index], Y[index]), (X[index] + W[index], Y[index] + H[index]),
+	    (0, 255, 0),1)
+    text = "SO FUNCIONA QUANDO QUER {}".format(aux+1)
+    cv2.putText(img, text, (X[index], Y[index]-10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    if aux >= 1:
+        return (printBox(index+1, X, Y, W, H, img, aux-1, dim, boxDrawing))
+    else:
+        return (dim[index])
+"""
 
 def convToBoundingBox(rectang): # Converte, para o formato (x, y, w, h), uma Bounding box encontrada
 
@@ -17,8 +39,8 @@ def convToBoundingBox(rectang): # Converte, para o formato (x, y, w, h), uma Bou
     return (x, y, w, h)
 
 def imageProp(img): # Imprime a imagem em array, o tipo de imagem e seu tamanho
-    print("Imagem: ")
-    print(img)
+    #print("Imagem: ")
+    #print(img)
 
     print("Tipo de imagem: ")
     print(type(img))
@@ -58,18 +80,27 @@ while True: # Reproduz vídeo até que uma tecla definida seja pressionada
             cv2.putText(resize, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
             	0.7, (0, 0, 255), 2)
 
+    boxNumber = []
     boxDim = []
     bX = []
     bY = []
     bW = []
     bH = []
+#    X = []
+#    Y = []
+#    W = []
+#    H = []
+
+
     for aux in range(0, len(detectRect)):
         boxDim.append(aux)
         bX.append(aux)
         bY.append(aux)
         bW.append(aux)
         bH.append(aux)
-    print(len(boxDim))
+
+    #print(len(boxDim))
+
     for boxDrawing in detectRect:
         for aux in range(0, len(detectRect)):
             boxDim[aux] = face_utils.rect_to_bb(boxDrawing)
@@ -77,8 +108,18 @@ while True: # Reproduz vídeo até que uma tecla definida seja pressionada
         #(bX, bY, bW, bH) = face_utils.rect_to_bb(boxDrawing)
 
             cv2.rectangle(resize, (bX[aux], bY[aux]), (bX[aux] + bW[aux], bY[aux] + bH[aux]),
-        	    (0, 255, 0), 1)
-
+        	    (0, 255, 0),1)
+            text = "SO FUNCIONA QUANDO QUER {}".format(aux+1)
+            cv2.putText(resize, text, (bX[aux], bY[aux]-10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+#        for j in range(0, len(detectRect)):
+#            boxNumber.append(len(detectRect))
+#            bX.append(len(detectRect))
+#            bY.append(len(detectRect))
+#            bW.append(len(detectRect))
+#            bH.append(len(detectRect))
+#            boxNumber[j] = printBox(0, X, Y, W, H, resize, j, boxDim, boxDrawing)
+#            (bX[j], bY[j], bW[j], bH[j]) = boxNumber[j]
         shape = predictor(gray, boxDrawing)
         shape = face_utils.shape_to_np(shape)
 
@@ -102,13 +143,14 @@ while True: # Reproduz vídeo até que uma tecla definida seja pressionada
 
     if roi:
         for i in range(0, len(roi)):
-            cv2.imshow("ROI: Rosto {}".format(i+1), roi[i])
+            cv2.imshow("Rosto {}".format(i+1), roi[i])
 
     k = cv2.waitKey(10) & 0xFF
     if k == ord('c'):
         break
 
-# imageProp(image)
+imageProp(roi[0])
+imageProp(roi[1])
 
 cv2.destroyAllWindows()
 cap.release()
