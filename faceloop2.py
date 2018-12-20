a9405666c2e2c4a98c9a0582a088d91f95b3d42f
Miguel Ravagnani <miguel.ravagnani@gmail.com>
2018-12-20 18:15:51 -0200

    faceloop 20/12/2018 (5)

--- faceloop2.py
+++ faceloop2.py
@@ -8,34 +8,34 @@ import cv2
 import dlib
 
 
-def saveCrop(img):
+def save_crop(img):
     for j in range(0, len(img)):
-        pathDir = os.path.dirname(os.path.abspath(__file__))
-        directory = pathDir + "/face_{}".format(j+1)
+        path_dir = os.path.dirname(os.path.abspath(__file__))
+        directory = path_dir + "/face_{}".format(j+1)
         if not os.path.exists(directory):
             os.makedirs(directory)
-        cv2.imwrite(os.path.join(directory , 'face_{}.jpg'.format(j+1)), img[j])
+        cv2.imwrite(os.path.join(directory, 'face_{}.jpg'.format(j+1)), img[j])
 
-def delCrop(img):
+def del_crop(img):
     for j in range(0, len(img)):
-        pathDir = os.path.dirname(os.path.abspath(__file__))
-        directory = pathDir + "/face_{}".format(j+1)
+        path_dir = os.path.dirname(os.path.abspath(__file__))
+        directory = path_dir + "/face_{}".format(j+1)
         shutil.rmtree(directory)
 
-def showCrop(img, j, aux):
+def show_crop(img, j, AUX):
     assert img[j] is not None
     cv2.imshow("Crop {}".format(j+1), img[j])
     cv2.moveWindow("Crop {}".format(j+1), 580, j*200)
-    if (aux-1) > 0:
-        showCrop(img, j+1, aux-1)
+    if (AUX-1) > 0:
+        show_crop(img, j+1, AUX-1)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-	help="Caminho para Shape Predictor")
-args = vars(ap.parse_args()) # shape_predictor_68_face_landmarks.dat
+AP = argparse.ArgumentParser()
+AP.add_argument("-p", "--shape-predictor", required=True,
+                help="Caminho para Shape Predictor")
+ARGS = vars(AP.parse_args()) # shape_predictor_68_face_landmarks.dat
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
+DETECTOR = dlib.get_frontal_face_detector()
+PREDICTOR = dlib.shape_predictor(ARGS["shape_predictor"])
 
 print("[TIME SLEEP]: 3s..")
 time.sleep(1.0)
@@ -44,43 +44,43 @@ time.sleep(1.0)
 print("[TIME SLEEP]: 1s..")
 time.sleep(1.0)
 
-cap = cv2.VideoCapture(0) # Caputa video
-cap.set(5,30)
+CAP = cv2.VideoCapture(0) # Caputa video
+CAP.set(5, 30)
 
 while True: # Reproduz video ate que uma tecla definida seja pressionada
-    ret, image = cap.read()
-    image = cv2.flip(image, 1, 0)
-    resize = cv2.resize(image, (240, 180))
-    display = cv2.resize(image, (480, 360))
-    gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
+    RET, IMAGE = CAP.read()
+    IMAGE = cv2.flip(IMAGE, 1, 0)
+    RESIZE = cv2.resize(IMAGE, (240, 180))
+    DISPLAY = cv2.resize(IMAGE, (480, 360))
+    GRAY = cv2.cvtColor(RESIZE, cv2.COLOR_BGR2GRAY)
 
-    detectRect = detector(gray, 1)
+    DETECT_RET = DETECTOR(GRAY, 1)
 
-    if len(detectRect) > 0:
-        text = "{} rosto(s) encontrado(s)".format(len(detectRect))
-        cv2.putText(display, text, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-        	0.7, (0, 0, 255), 2)
+    if len(DETECT_RET) > 0:
+        TEXT = "{} rosto(s) encontrado(s)".format(len(DETECT_RET))
+        cv2.putText(DISPLAY, TEXT, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+            	0.7, (0, 0, 255), 2)
 
-    crop = []
-    for i, dim in enumerate(detectRect):
+    CROP = []
+    for i, dim in enumerate(DETECT_RET):
         print("Rosto {}: Left: {} Top: {} Right: {} Bottom: {}".format(
             i, dim.left(), dim.top(), dim.right(), dim.bottom()))
-        crop.append(i)
-        crop[i] = resize[max(0, dim.top()): min(dim.bottom(), 480),
-                    max(0, dim.left()): min(dim.right(), 360)]
+        CROP.append(i)
+        CROP[i] = RESIZE[max(0, dim.top()): min(dim.bottom(), 480),
+                         max(0, dim.left()): min(dim.right(), 360)]
 
-        cv2.rectangle(display, (dim.left()*2, dim.top()*2), (dim.left()*2 + dim.right()*2 - dim.left()*2, dim.top()*2 + dim.bottom()*2 - dim.top()*2), (0, 255, 0), 1)
+        cv2.rectangle(DISPLAY, (dim.left()*2, dim.top()*2), (dim.left()*2 + dim.right()*2 - dim.left()*2, dim.top()*2 + dim.bottom()*2 - dim.top()*2), (0, 255, 0), 1)
 
-        text = "Rosto {}".format(i+1)
-        cv2.putText(display, text, (dim.left()*2, dim.top()*2-10),
+        TEXT = "Rosto {}".format(i+1)
+        cv2.putText(DISPLAY, TEXT, (dim.left()*2, dim.top()*2-10),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
-    cv2.imshow("Video Output - 480x360", display)
+    cv2.imshow("Video Output - 480x360", DISPLAY)
     cv2.moveWindow("Video Output - 480x360", 0, 0)
 
-    if len(crop) > 0:
-        showCrop(crop, 0, len(crop))
-        saveCrop(crop)
+    if len(CROP) > 0:
+        show_crop(CROP, 0, len(CROP))
+        save_crop(CROP)
 
     k = cv2.waitKey(10) & 0xFF
     if k == ord('c'):
@@ -90,17 +90,17 @@ cv2.destroyAllWindows()
 
 time.sleep(1.0)
 
-if len(crop) > 0:
-    delet = ''
+if len(CROP) > 0:
+    DELET = ''
     while True:
-        delet = input("Manter pasta(s) criada(s)? [s/n] ")
-        if delet == 's' or delet == 'n':
+        DELET = input("Manter pasta(s) criada(s)? [s/n] ")
+        if DELET == 's' or DELET == 'n':
             break
 
-    if delet == 'n':
-        delCrop(crop)
+    if DELET == 'n':
+        del_crop(CROP)
         print("Pasta(s) apagada(s)")
     else:
         print("Pasta(s) conservada(s)")
 
-cap.release()
+CAP.release()
